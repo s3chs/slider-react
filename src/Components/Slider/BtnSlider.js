00dff81eb592ab68841462b00d347ba826283fd5
sechs <philippe.buteau@skema.edu>
2021-07-03 13:09:59 +0200

    slider finished

---
+++ src/Components/Slider/BtnSlider.js
@@ -0,0 +1,20 @@
+import React from "react";
+import leftArrow from "./icons/left-arrow.svg";
+import rightArrow from "./icons/right-arrow.svg";
+
+export default function BtnSlider({ direction, moveSlide }) {
+  return (
+    <div>
+      <button
+        onClick={moveSlide}
+        className={direction === "next" ? "btn-slide next" : "btn-slide prev"}
+      >
+        {" "}
+        <img
+          src={direction === "next" ? rightArrow : leftArrow}
+          alt="arrow-icon"
+        />
+      </button>
+    </div>
+  );
+}
